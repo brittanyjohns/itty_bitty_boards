4dd36ba81b1c1a312e3ea7635936959e8cd878f9
Brittany Johns <bhannajohns@gmail.com>
2024-01-15 14:54:47 -0500

    added clear btn

--- app/javascript/controllers/notice_controller.js
+++ app/javascript/controllers/notice_controller.js
@@ -5,10 +5,23 @@ export default class extends Controller {
   connect() {
     console.log("Hello, Notice!", this.element)
     const notice = document.querySelector("#notice")
+    this.theList = document.querySelector("#the-list")
+    console.log("this.theList: ", this.theList)
+
     if (notice) {
       setTimeout(() => {
         notice.classList.add("hidden")
       }, 3000)
     }
+  }
+
+  clearList() {
+    console.log("Clearing list")
+    this.theList.innerHTML = ""
+    const highlighted = document.querySelectorAll(".bg-green-200")
+    highlighted.forEach((item) => {
+      item.classList.remove("bg-green-200")
+      item.classList.add("bg-white")
+    })
   }
 }
