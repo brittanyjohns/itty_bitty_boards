0e777ba3c1f24b95f4c9d51cd08404ff923a54ae
Brittany Johns <bhannajohns@gmail.com>
2024-01-16 17:43:55 -0500

    fix cart show error

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -8,19 +8,23 @@ class CartsController < ApplicationController
     raise "No order found" unless @current_order
 
     @order_items = @current_order.order_items
+    redirect_to root_path, notice: "Your cart is empty" if @order_items.empty?
+
     @amount = @current_order.total
     @stripe_customer_id = current_user.stripe_customer_id
     current_user.set_payment_processor :stripe
-    @checkout_session = current_user.payment_processor.checkout_charge(
-      amount: 100,
-      name: "One-time payment for #{current_user.email}",
-      currency: "usd",
-      quantity: @amount.to_i,
-      success_url: DOMAIN + '/success',
-      cancel_url: DOMAIN + '/cancel'
-    )
-    current_user.stripe_customer_id = @checkout_session.customer
-    current_user.save!
+    if @amount > 0
+      @checkout_session = current_user.payment_processor.checkout_charge(
+        amount: 100,
+        name: "One-time payment for #{current_user.email}",
+        currency: "usd",
+        quantity: @amount.to_i,
+        success_url: DOMAIN + '/success',
+        cancel_url: DOMAIN + '/cancel'
+      )
+      current_user.stripe_customer_id = @checkout_session.customer
+      current_user.save!
+    end
   end
 
 end
