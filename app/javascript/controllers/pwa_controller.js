fc90524a85fdfdcbb033bfab5bde91c9c6cbfd6a
Brittany Johns <bhannajohns@gmail.com>
2024-01-23 16:10:18 -0500

    fix

--- app/javascript/controllers/pwa_controller.js
+++ app/javascript/controllers/pwa_controller.js
@@ -4,35 +4,18 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["button"]
   connect() {
-    // let elem = document.documentElement; // Get the documentElement (<html>) to display the page in fullscreen
-    // let isFullscreen = document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement || document.msFullscreenElement;
-    // if (isFullscreen) {
-    //   if (document.exitFullscreen) {
-    //     document.exitFullscreen();
-    //   } else if (document.webkitExitFullscreen) { /* Safari */
-    //     document.webkitExitFullscreen();
-    //   } else if (document.msExitFullscreen) { /* IE11 */
-    //     document.msExitFullscreen();
-    //   }
-    // } else {
-    //   if (elem.requestFullscreen) {
-    //     elem.requestFullscreen();
-    //   } else if (elem.webkitRequestFullscreen) { /* Safari */
-    //     elem.webkitRequestFullscreen();
-    //   } else if (elem.msRequestFullscreen) { /* IE11 */
-    //     elem.msRequestFullscreen();
-    //   }
-    // }
+    if (window.matchMedia('(display-mode: standalone)').matches) {
+      console.log('display-mode is standalone');
+      this.buttonTarget.style.display = "none";
+    }
   }
 
   fullscreen() {
-    console.log('fullscreen');
     let elem = document.documentElement; // Get the documentElement (<html>) to display the page in fullscreen
     this.isFullscreen = document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement || document.msFullscreenElement;
 
     if (this.isFullscreen) {
-      console.log('exit fullscreen');
-      this.buttonTarget.innerHTML = '<i class="fas fa-expand"></i> Enter Fullscreen';
+      this.buttonTarget.innerHTML = '<i class="fas fa-expand"></i>';
       if (document.exitFullscreen) {
         document.exitFullscreen();
       } else if (document.webkitExitFullscreen) { /* Safari */
@@ -41,8 +24,7 @@ export default class extends Controller {
         document.msExitFullscreen();
       }      
     } else {
-      console.log('enter fullscreen');
-      this.buttonTarget.innerHTML = '<i class="fas fa-compress"></i> Exit Fullscreen';
+      this.buttonTarget.innerHTML = '<i class="fas fa-compress"></i>';
       if (elem.requestFullscreen) {
         elem.requestFullscreen();
       } else if (elem.webkitRequestFullscreen) { /* Safari */
