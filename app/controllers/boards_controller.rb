5fd4cc1290078e4cd576ad1d7b5331886869d6b0
Brittany Johns <bhannajohns@gmail.com>
2024-02-05 22:20:31 -0500

    edit predef

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -158,6 +158,6 @@ class BoardsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def board_params
-      params.require(:board).permit(:user_id, :name, :parent_id, :parent_type, :description, :number_of_columns)
+      params.require(:board).permit(:user_id, :name, :parent_id, :parent_type, :description, :number_of_columns, :predefined)
     end
 end
