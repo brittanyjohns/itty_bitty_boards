6ea0beffe6a5e054d45340cf6ede0c52cd0cf134
Brittany Johns <bhannajohns@gmail.com>
2024-02-11 08:19:03 -0500

    nav icons

--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -18,4 +18,16 @@ module IconHelper
     def home_nav(tooltip_text = "Home")
         "<i class='fa-solid fa-home fa-lg' data-action='mouseover->tooltip#mouse', data-tooltip='#{tooltip_text}' }></i>".html_safe
     end
+
+    def menu_nav(tooltip_text = "Menus")
+        "<i class='fa-solid fa-apple-whole fa-lg' data-action='mouseover->tooltip#mouse', data-tooltip='#{tooltip_text}' }></i>".html_safe
+    end
+
+    def image_nav(tooltip_text = "Images")
+        "<i class='fa-solid fa-image fa-lg' data-action='mouseover->tooltip#mouse', data-tooltip='#{tooltip_text}' }></i>".html_safe
+    end
+
+    def board_nav(tooltip_text = "Boards")
+        "<i class='fa-solid fa-clipboard-list fa-lg' data-action='mouseover->tooltip#mouse', data-tooltip='#{tooltip_text}' }></i>".html_safe
+    end
 end
