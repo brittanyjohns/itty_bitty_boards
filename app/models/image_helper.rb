2bbb0908a08d684d90c0a6240121a7e6e79ce08d
Brittany Johns <bhannajohns@gmail.com>
2024-04-17 21:56:54 -0400

    typo

--- app/models/image_helper.rb
+++ app/models/image_helper.rb
@@ -11,7 +11,7 @@ module ImageHelper
       #                             "User-Agent" => "Ruby/#{RUBY_VERSION}",
       #                             "From" => "foo@bar.invalid",
       #                             "Referer" => "http://www.ruby-lang.org/")
-      downloaded_img = Down.download(url)
+      downloaded_image = Down.download(url)
       user_id ||= self.user_id
       doc = self.docs.create!(raw: name_to_send, user_id: user_id, processed: revised_prompt, source_type: "OpenAI")
       doc.image.attach(io: downloaded_image, filename: "img_#{self.id}_doc_#{doc.id}.webp", content_type: "image/webp")
