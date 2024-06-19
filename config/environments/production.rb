f9d97850a16821fdbdbe25e42c6ddf0e90ff115a
Brittany Johns <brittanyjohns@admins-MBP.lan>
2024-06-19 14:41:31 -0400

    oops

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -114,11 +114,11 @@ Rails.application.configure do
 #     authentication: "plain",
 #     enable_starttls_auto: true
 #   }
-# end
 
-config.action_mailer.delivery_method = :mailgun
-config.action_mailer.mailgun_settings = {
-  api_key: ENV['MAILGUN_API_KEY'],
-  domain: 'mail.speakanyway.com',
-  # timeout: 20 # Default depends on rest-client, whose default is 60s. Added in 1.2.3.
-}
+  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.mailgun_settings = {
+    api_key: ENV['MAILGUN_API_KEY'],
+    domain: 'mail.speakanyway.com',
+    # timeout: 20 # Default depends on rest-client, whose default is 60s. Added in 1.2.3.
+  }
+end
