fc8ecb2f41d9da05754808281d515736a4e99f17
Brittany Johns <brittanyjohns@admins-MacBook-Pro.local>
2024-08-28 17:25:45 -0400

    working

--- app/controllers/api/google_search_results_controller.rb
+++ app/controllers/api/google_search_results_controller.rb
@@ -19,18 +19,31 @@ class API::GoogleSearchResultsController < API::ApplicationController
 
   def image_search
     puts "\n**google_images endpoint hit**\n"
-    search_results = GoogleResultsService.new(params[:q]).search
-    render json: search_results
+    image_search_service = GoogleResultsService.new(params[:q])
+    image_search_service.search
+    # search_results = image_search_service.images_api_view
+    image_results = image_search_service.images_api_view
+
+    render json: image_results
   end
 
+  #   export interface ImageResult {
+  #   link: string;
+  #   title: string;
+  #   thumbnail: string;
+  #   snippet?: string;
+  #   context?: string;
+  # }
+
   def save_image_result
-    puts "\n**save_image_result endpoint hit**\n"
-    src = params[:src]
-    title = params[:title]
-    long_title = params[:long_title]
+    puts "\n**save_image_result endpoint hit**\n #{params.inspect}"
+    src = params[:imageResult][:link]
+    title = params[:query]
+    long_title = params[:imageResult][:title]
+    file_format = params[:imageResult][:fileFormat]
     user_id = current_user.id
-    puts "Saving image result: #{src}, #{title}, #{long_title}, #{user_id}"
-    image = Image.create_image_from_google_search(src, title, long_title, user_id)
+    puts "Saving image result: #{src}, #{title}, #{long_title}, #{user_id} , #{file_format}"
+    image = Image.create_image_from_google_search(src, title, long_title, file_format, user_id)
     render json: image
   end
 end
