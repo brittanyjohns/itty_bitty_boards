898ee3e5775cdef18b97d2d893a9f739e8ff9ddd
Brittany Johns <brittanyjohns@admins-MacBook-Pro.local>
2024-10-26 10:26:00 -0400

    words2

--- app/models/board.rb
+++ app/models/board.rb
@@ -475,6 +475,10 @@ class Board < ApplicationRecord
     ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]
   end
 
+  def words
+    images.map(&:label)
+  end
+
   def api_view_with_images(viewing_user = nil)
     {
       id: id,
