8829ad194befdbe66ef90e083cb1a32a9c06c282
Brittany Johns <brittanyjohns@admins-MacBook-Pro.local>
2024-12-30 11:04:30 -0500

    devise

--- app/models/turbo/devise_failure_app.rb
+++ app/models/turbo/devise_failure_app.rb
@@ -1,28 +1,28 @@
-module Turbo
-    class DeviseFailureApp < Devise::FailureApp
-      class << self
-          def helper_method(name)
-          end
-      end
-      include Turbo::Native::Navigation
-  
-      def respond
-        if request_format == :turbo_stream
-          redirect
-        elsif turbo_native_app?
-          http_auth
-        else
-          super
-        end
-      end
-  
-      def skip_format?
-        %w[html turbo_stream */*].include? request_format.to_s
-      end
+# module Turbo
+#     class DeviseFailureApp < Devise::FailureApp
+#       class << self
+#           def helper_method(name)
+#           end
+#       end
+#       include Turbo::Native::Navigation
 
-      # Turbo Native requests that require authentication should return 401s to trigger the login modal
-      def http_auth?
-          turbo_native_app? || super
-      end
-    end
-end
+#       def respond
+#         if request_format == :turbo_stream
+#           redirect
+#         elsif turbo_native_app?
+#           http_auth
+#         else
+#           super
+#         end
+#       end
+
+#       def skip_format?
+#         %w[html turbo_stream */*].include? request_format.to_s
+#       end
+
+#       # Turbo Native requests that require authentication should return 401s to trigger the login modal
+#       def http_auth?
+#           turbo_native_app? || super
+#       end
+#     end
+# end
