61074ca373cca389e5b932233dbdcbc25610a790
Brittany Johns <brittanyjohns@admins-MacBook-Pro.local>
2024-12-30 17:29:46 -0500

    script

--- lib/scripts/fix_metadata.rb
+++ lib/scripts/fix_metadata.rb
@@ -2,16 +2,42 @@ require "aws-sdk-s3"
 
 s3 = Aws::S3::Client.new(region: "us-east-1")
 
-bucket_name = "itty-bitty-boards-development"
+bucket_name = "itty-bitty-boards-#{Rails.env}"
 key = "e00epy1omnmnmjqcq9z0oqsotlu5"
 
-s3.copy_object(
-  bucket: bucket_name,
-  copy_source: "#{bucket_name}/#{key}",
-  key: key,
-  metadata_directive: "REPLACE",
-  content_type: "image/svg+xml",
-  content_disposition: "inline",
-)
+# s3.copy_object(
+#   bucket: bucket_name,
+#   copy_source: "#{bucket_name}/#{key}",
+#   key: key,
+#   metadata_directive: "REPLACE",
+#   content_type: "image/svg+xml",
+#   content_disposition: "inline",
+# )
+keys = []
+# List objects in the bucket
+s3.list_objects_v2(bucket: bucket_name).contents.each do |object|
+  key = object.key
+  metadata = s3.head_object(bucket: bucket_name, key: key)
+  if metadata.content_type == "application/octet-stream"
+    puts "Key with wrong content-type: #{key}"
+    keys << key
+  end
+end
 
-puts "Metadata updated successfully!"
+# # write the keys to a file
+# File.open("keys.txt", "w") { |file| file.write(keys.join("\n")) }
+# puts "Keys written to keys.txt"
+# `open keys.txt`
+
+keys.each do |key|
+  s3.copy_object(
+    bucket: bucket_name,
+    copy_source: "#{bucket_name}/#{key}",
+    key: key,
+    metadata_directive: "REPLACE",
+    content_type: "image/svg+xml",
+    content_disposition: "inline",
+  )
+  puts "Updated key: #{key}"
+end
+puts "Done"
