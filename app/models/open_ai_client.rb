60ca4bc77b1cf1e3c220fcde1d2b2dee85113027
Brittany Johns <brittanyjohns@admins-MacBook-Pro.local>
2025-06-16 16:09:41 -0400

    clean up

--- app/models/open_ai_client.rb
+++ app/models/open_ai_client.rb
@@ -203,10 +203,8 @@ class OpenAiClient
                                                               text: "This is a restaurant menu. Please describe the menu items in a list like your were reading them to the server. Please respond as json in the following format: #{expected_json_schema}" },
                                                             { type: "image_url", image_url: { url: img_url } }] }],
                                     })
-      puts "Menu OPENAI Response: #{response}"
       Rails.logger.debug "*** ERROR *** Invaild Menu Description Response: #{response}" unless response
     rescue => e
-      puts "**** ERROR **** \n#{e.message}\n#{e.inspect}"
       Rails.logger.debug "**** ERROR **** \n#{e.message}\n#{e.inspect}"
     end
     # save_response_locally(response)
