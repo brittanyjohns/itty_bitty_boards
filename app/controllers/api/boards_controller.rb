6a37c5671327474e383a06e886f3c5c02dd0db66
Brittany Johns <brittanyjohns@admins-MacBook-Pro.local>
2025-08-03 20:33:28 -0400

    wip

--- app/controllers/api/boards_controller.rb
+++ app/controllers/api/boards_controller.rb
@@ -16,9 +16,6 @@ class API::BoardsController < API::ApplicationController
     unless current_user
       @static_preset_boards = Board.predefined.order(name: :asc).page params[:page]
       render json: { static_preset_boards: @static_preset_boards.map(&:api_view),
-                     dynamic_preset_boards: @dynamic_preset_boards.map(&:api_view),
-                     predictive_preset_boards: @predictive_preset_boards.map(&:api_view),
-                     category_preset_boards: @category_preset_boards.map(&:api_view),
                      preset_boards: @static_preset_boards.map(&:api_view) }
       return
     end
