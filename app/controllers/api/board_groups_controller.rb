787846189b6543d713c768c564607165f0e53d19
Brittany Johns <brittanyjohns@admins-MacBook-Pro.local>
2025-08-04 22:12:54 -0400

    items

--- app/controllers/api/board_groups_controller.rb
+++ app/controllers/api/board_groups_controller.rb
@@ -142,7 +142,7 @@ class API::BoardGroupsController < API::ApplicationController
     end
     if board_group.save
       mark_default(board_group)
-      save_layout!
+      save_layout! if params[:layout].present?
       Rails.logger.debug "Board Group updated successfully: #{board_group.id}"
       render json: board_group.api_view_with_boards(current_user)
     else
