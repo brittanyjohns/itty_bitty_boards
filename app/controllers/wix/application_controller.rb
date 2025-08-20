f5422cbfcf839348e043ae2bf98fe0ece22237d5
Brittany Johns <brittanyjohns@admins-MacBook-Pro.local>
2025-08-20 17:50:11 -0400

    perf & wix

--- app/controllers/wix/application_controller.rb
+++ app/controllers/wix/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::Base
+class Wix::ApplicationController < ActionController::Base
   def submit
     Rails.logger.info("Submitting form with params: #{params.inspect}")
     # Handle form submission logic here
