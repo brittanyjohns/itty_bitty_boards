964d11cfd12ca007dd20f7c34c59362fafddffa0
Brittany Johns <brittanyjohns@admins-MacBook-Pro.local>
2025-08-20 17:43:33 -0400

    perf & wix

--- app/controllers/wix/application_controller.rb
+++ app/controllers/wix/application_controller.rb
@@ -1,14 +1,4 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
-  before_action :set_locale
-
-  private
-
-  def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
-  end
-
   def submit
     Rails.logger.info("Submitting form with params: #{params.inspect}")
     # Handle form submission logic here
