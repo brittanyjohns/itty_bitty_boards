b787b67ef3e08fe1417892278edb97a7ac7090c6
Brittany Johns <brittanyjohns@admins-MacBook-Pro.local>
2025-11-21 12:18:42 -0500

    clean up log

--- app/controllers/api/images_controller.rb
+++ app/controllers/api/images_controller.rb
@@ -364,7 +364,6 @@ class API::ImagesController < API::ApplicationController
     @image = Image.find(params[:id])
     board_id = params[:board_id]
     @board = Board.with_artifacts.find_by(id: board_id) if board_id.present?
-    Rails.logger.info("Creating predictive board for image: #{@image.label} -- board_id: #{board_id}")
     unless @board.nil?
       @board_image = @board.board_images.find_by(image_id: @image.id)
       if @board_image.nil?
