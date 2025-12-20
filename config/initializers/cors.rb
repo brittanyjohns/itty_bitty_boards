63d4fb93b9beed48bdb711afc0a32679c9e89330
Brittany Johns <brittanyjohns@admins-MacBook-Pro.local>
2025-12-20 12:43:38 -0500

    add correct domain

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:8100", "https://speakanyway.com", "http://www.speakanyway.com"
+    origins "http://localhost:8100", "https://speakanyway.com", "http://www.speakanyway.com", "https://app.speakanyway.com", "http://app.speakanyway.com"
     # origins "*"
 
     resource "*",
