e8b210f22d6e1ee53a148b0216083af4c8c84ff1
Brittany Johns <brittanyjohns@admins-MacBook-Pro.local>
2026-01-12 14:40:37 -0500

    fix editing

--- app/controllers/api/board_images_controller.rb
+++ app/controllers/api/board_images_controller.rb
@@ -298,6 +298,6 @@ class API::BoardImagesController < API::ApplicationController
                                         :text_color, :font_size, :border_color,
                                         :display_label,
                                         :label,
-                                        :layout, :status, :audio_url, :hidden, :src, :display_image_url)
+                                        :layout, :audio_url, :hidden, :src, :display_image_url)
   end
 end
