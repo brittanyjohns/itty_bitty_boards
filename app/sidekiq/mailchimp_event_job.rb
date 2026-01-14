a2d4c2c5f5908f2cd90bf51ba6e4114ded1713fa
Brittany Johns <brittanyjohns@admins-MacBook-Pro.local>
2026-01-14 14:25:33 -0500

    sign up

--- app/sidekiq/mailchimp_event_job.rb
+++ app/sidekiq/mailchimp_event_job.rb
@@ -11,6 +11,9 @@ class MailchimpEventJob
     case event_type
     when "sign_in"
       mailchimp.record_signin_event(user, options)
+    when "sign_up"
+      tags = options[:tags] || []
+      mailchimp.record_new_subscriber(user, tags: tags)
     else
       Rails.logger.warn("Unknown Mailchimp event type: #{event_type}")
     end
